4590f3738fb8f9e740f6a673749de2e35cd2e1ce
vinod-45-vinod <vinodgr2005@gmail.com>
2025-10-16 12:57:58 +0530

    Task 4 completed and Fixed Ai paddle smooth movement

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame
 from game.game_engine import GameEngine
+import os
 
-# Initialize pygame/Start application
+# Initialize pygame
 pygame.init()
+pygame.mixer.init()
 
-# Screen dimensions
+# Screen
 WIDTH, HEIGHT = 800, 600
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Ping Pong - Pygame Version")
@@ -17,8 +19,23 @@ BLACK = (0, 0, 0)
 clock = pygame.time.Clock()
 FPS = 60
 
-# Game loop
-engine = GameEngine(WIDTH, HEIGHT)
+# Load sounds
+def load_sound(filename):
+    path = os.path.join("assets", filename)
+    if os.path.exists(path):
+        return pygame.mixer.Sound(path)
+    else:
+        print(f"Warning: {filename} not found!")
+        return None
+
+paddle_sound = load_sound(r"C:\Users\Vinod G R\OneDrive\Desktop\lab4\ping-pong\assets\paddle.wav")
+wall_sound = load_sound(r"C:\Users\Vinod G R\OneDrive\Desktop\lab4\ping-pong\assets\wall.wav")
+score_sound = load_sound(r"C:\Users\Vinod G R\OneDrive\Desktop\lab4\ping-pong\assets\score.wav")
+# Create GameEngine
+engine = GameEngine(WIDTH, HEIGHT,
+                    paddle_sound=paddle_sound,
+                    wall_sound=wall_sound,
+                    score_sound=score_sound)
 
 def main():
     running = True
